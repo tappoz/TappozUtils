4700425bcf6290877e84248912c446ad10b3e1dc
tappoz <tappoz84@yahoo.it>
2013-11-15 18:31:12 +0000

    more meaningful Unit Tests for the boundaries of the ranges

--- test/org/tappoz/amounts/TaxRatesUtilUnitTest.java
+++ test/org/tappoz/amounts/TaxRatesUtilUnitTest.java
@@ -6,14 +6,28 @@ import org.junit.Test;
 
 public class TaxRatesUtilUnitTest
 {
-
 	@Test
-	public void testTaxRangesUpperBound()
+	public void test_getTaxRangesUpperBound()
 	{
+		// in the first range
 		assertEquals(1, TaxRatesUtil.getTaxRangesUpperBound(100));
+		// in the second range
 		assertEquals(2, TaxRatesUtil.getTaxRangesUpperBound(7000));
+	}
+	
+	@Test
+	public void test_getTaxRangesAboveTheLastLowerBound()
+	{
+		// above the last lower bound (i.e. in the last range)
 		assertEquals(6, TaxRatesUtil.getTaxRangesUpperBound(50000));
+		// the upper bound (included) of the 4th range
 		assertEquals(4, TaxRatesUtil.getTaxRangesUpperBound(21240));
 	}
 
+	@Test
+	public void test_getTaxRangesInABoundaryBetweenTwoDifferentRanges()
+	{
+		// the upper bound (included) of the 4th range
+		assertEquals(4, TaxRatesUtil.getTaxRangesUpperBound(21240));
+	}
 }
