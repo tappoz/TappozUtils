ce01fdb1b71bcc12adc8bbd33176fd39b304f0a3
tappoz <tappoz84@yahoo.it>
2013-11-16 16:11:29 +0000

    new test included, recursive method fixed

--- src/org/tappoz/amounts/TaxRatesUtil.java
+++ src/org/tappoz/amounts/TaxRatesUtil.java
@@ -4,14 +4,33 @@ import java.util.Arrays;
 
 public class TaxRatesUtil
 {
-	private static int taxRanges[] = {0, 5070, 8660, 14070, 21240, 40230};
+	private static double taxRanges[] = {0, 5000, 9000, 15000, 22000, 40000};
 	private static double taxRates[] = {0, 0.1, 0.14, 0.23, 0.3, 0.33, 0.45};
+	private static double taxRatesAmounts[] = {0, 0, 0, 0, 0, 0, 0};
 	
-	public static int getTaxRangesUpperBound(int amount){
+	public static int getTaxRangesUpperBound(double amount){
+		
+		if (amount < 0)
+			throw new UnsupportedOperationException("The amount must be greater than zero!");
 		
 		int index = Arrays.binarySearch(taxRanges, amount);
-		if (index >= 0) return index;
-		// http://docs.oracle.com/javase/1.4.2/docs/api/java/util/Arrays.html#binarySearch%28int[],%20int%29
-		else return - index - 1;
+		if (index > 0) return index;
+		else return - index - 1; // cfr. official documentation of the binary search for this 
 	}
+	
+	public static double getTaxesAmount(double amount)
+	{	
+    	int index = getTaxRangesUpperBound(amount);
+    	
+    	if (index == 1)
+    	{
+    		return amount * taxRates[index];
+    	}
+    	else
+    	{
+    		double amountInCurrentRange = amount - taxRanges[index - 1];
+    		// recursion here with the left amount on which the taxes will be computed
+    		return amountInCurrentRange * taxRates[index] + getTaxesAmount(taxRanges[index - 1]);
+    	}
+    }
 }
--- test/org/tappoz/amounts/TaxRatesUtilUnitTest.java
+++ test/org/tappoz/amounts/TaxRatesUtilUnitTest.java
@@ -30,4 +30,31 @@ public class TaxRatesUtilUnitTest
 		// the upper bound (included) of the 4th range
 		assertEquals(4, TaxRatesUtil.getTaxRangesUpperBound(21240));
 	}
+	
+	// --------------------------------------------
+	
+	@Test
+	public void test_getTaxesAmountInFirstRange()
+	{
+		double amount = 1000;
+		// the 3rd parameter is the delta
+		assertEquals(100, TaxRatesUtil.getTaxesAmount(amount), 0.1);
+	}
+	
+	@Test
+	public void test_getTaxesAmountInAllRanges()
+	{
+		double amount = 50000;
+		// 10000 * 0.45 = 4500
+		// 18000 * 0.33 = 5940
+		//  7000 * 0.3  = 2100
+		//  6000 * 0.23 = 1380
+		//  4000 * 0.14 =  560
+		//  5000 * 0.1  =  500
+		//
+		// total: 14980
+		
+		// the 3rd parameter is the delta
+		assertEquals(14980, TaxRatesUtil.getTaxesAmount(amount), 0.1);
+	}
 }
